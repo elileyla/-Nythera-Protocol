e9c6010dcf8f33ff607579965354896b53e57591
elileyla <elileyla39@gmail.com>
2026-02-25 12:17:00 +0330

    Create mod.rs

---
+++ node/consensus/mod.rs
@@ -0,0 +1,11 @@
+pub struct ConsensusEngine;
+
+impl ConsensusEngine {
+    pub fn new() -> Self {
+        ConsensusEngine
+    }
+
+    pub fn start(&self) {
+        println!("Consensus engine started.");
+    }
+}
